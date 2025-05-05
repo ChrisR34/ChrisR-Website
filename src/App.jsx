2bf74c18e46f64c555a6ee9187574b3cfe32c050
Christos R <131167278+ChrisR34@users.noreply.github.com>
2025-05-05 11:12:03 +0000

    starting up

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Christos Rosis",
+  title: "Web Designer & Software Engineer",
+  email: "christosrosis@yahoo.com",
+  gitHub: "chrisR34",
+  instagram: "christos.rosis",
+  linkedIn: "christos-rosis-b32170347",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
